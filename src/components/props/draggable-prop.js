726653fec73e5134763910b9df26154e425a50e1
siavash <siavash.kavousi.sk@gmail.com>
2017-10-05 17:53:05 +0330

    style: draggable prop

--- src/components/props/draggable-prop.js
+++ src/components/props/draggable-prop.js
@@ -1,6 +1,6 @@
+import { getPropChildId, getWidget } from '@/utils'
 import SchemaProp from './schema-prop'
 import arrayMixin from '../mixins/array-mixin'
-import { getWidget } from '@/utils'
 
 export default {
   name: 'draggable-prop',
@@ -15,35 +15,57 @@ export default {
           list: this.value,
         },
       }, [
-        ...this.items.map(item => {
-          return h(SchemaProp, {
-            props: {
-              schema: item.schema,
-              uiSchema: item.uiSchema,
-              errorSchema: item.errorSchema,
-              idSchema: item.idSchema,
-              required: this.isItemRequired(item.schema),
-              value: item.value,
-              registry: this.registry,
-            },
-            on: {
-              input: itemVal => {
-                this.value.splice(item.index, 1, itemVal)
-                this.$emit('input', this.value)
+        ...this.items.map((item, index) => {
+          return h('div', {
+            'class': 'd-flex flex-column border border-muted p-2',
+          },[
+            h('div', {
+              'class': 'fa fa-times align-self-end text-danger',
+              on: {
+                click: this.onDropIndexClick.bind(null, index),
               },
-              blur: itemVal => {
-                this.value.splice(item.index, 1, itemVal)
-                this.$emit('blur', this.value)
-              }
-            },
-          }, item)
+            }),
+            h(SchemaProp, {
+              props: {
+                schema: item.schema,
+                uiSchema: item.uiSchema,
+                errorSchema: item.errorSchema,
+                idSchema: item.idSchema,
+                required: this.isItemRequired(item.schema),
+                value: item.value,
+                registry: this.registry,
+              },
+              on: {
+                input: itemVal => {
+                  this.value.splice(item.index, 1, itemVal)
+                  this.$emit('input', this.value)
+                },
+                blur: itemVal => {
+                  this.value.splice(item.index, 1, itemVal)
+                  this.$emit('blur', this.value)
+                }
+              },
+            }, item),
+          ])
         }),
         this.canAddItem(this.value) ? h('div', {
-          'class': 'btn btn-primary fa fa-plus',
+          'class': 'btn btn-primary fa fa-plus mt-2',
           on: {
             click: this.onAddClick,
           },
-        }) : undefined
+        }) : undefined,
+        h('div', {
+          attrs: {
+            id: getPropChildId(this.idSchema, 'label'),
+          },
+          slot: 'label'
+        }, this.uiOptions.label),
+        h('div', {
+          attrs: {
+            id: getPropChildId(this.idSchema, 'description'),
+          },
+          slot: 'description'
+        }, this.uiOptions.description)
       ])
   }
 }
